d6086046064f35046549f895ac44cac49a610c1d
wenjianfang <wenjianfang0821@163.com>
2022-03-18 18:57:29 +0800

    second commit

--- src/main/java/dhu/edu/cn/git/GitTest.java
+++ src/main/java/dhu/edu/cn/git/GitTest.java
@@ -7,6 +7,7 @@ package dhu.edu.cn.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git !!!");
+        // System.out.println("hello git !!!");
+        System.out.println("hello git 2 !!!");
     }
 }
