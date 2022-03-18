b5602659b39c38809f18cd0726ede56dce12ac22
wenjianfang <wenjianfang0821@163.com>
2022-03-18 19:02:18 +0800

    third commit

--- src/main/java/dhu/edu/cn/git/GitTest.java
+++ src/main/java/dhu/edu/cn/git/GitTest.java
@@ -7,7 +7,8 @@ package dhu.edu.cn.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        // System.out.println("hello git !!!");
+        System.out.println("hello git !!!");
         System.out.println("hello git 2 !!!");
+        System.out.println("hello git 3 !!!");
     }
 }
